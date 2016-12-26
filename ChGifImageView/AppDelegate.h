980c0e2e0a0a6eb0b11e4db33c690d7fab62addf
Ashuro <13591364646@163.com>
2016-12-26 11:05:24 +0800

    Initial Commit

---
+++ ChGifImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChGifImageView
+//
+//  Created by Ashuro on 2016/12/26.
+//  Copyright © 2016年 Ashuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChGifImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChGifImageView
+//
+//  Created by Ashuro on 2016/12/26.
+//  Copyright © 2016年 Ashuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
